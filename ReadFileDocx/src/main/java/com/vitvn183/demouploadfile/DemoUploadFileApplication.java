3338f1f2de74e5836c7120ea7a5f8eecc450974f
Nguyễn Văn Việt <vanvietgg183@gmail.com>
2022-08-04 14:55:28 +0700

    btvn b2

---
+++ ReadFileDocx/src/main/java/com/vitvn183/demouploadfile/DemoUploadFileApplication.java
@@ -0,0 +1,15 @@
+package com.vitvn183.demouploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class DemoUploadFileApplication {
+
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(DemoUploadFileApplication.class, args);
+    }
+
+}
